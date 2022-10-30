cf5c49af139d57b3a9c8566adc83388a099cc57b
Chirag Dogra <chirag.dogra79@gmail.com>
2022-10-30 21:29:55 +0530

    adding google analytics

--- components/Typing/index.js
+++ components/Typing/index.js
@@ -3,7 +3,7 @@ import Typical from 'react-typical';
 
 function Typing() {
   return (
-    <div className="mt-5 text-3xl mob:text-2xl laptop:text-6xl mob:p-2  mob:w-full laptop:w-full  mob: text-center">
+    <div className="mt-5 text-3xl mob:text-2xl laptop:text-6xl mob:p-2  mob:w-full laptop:w-full  mob: text-center ">
         I am {' '}
         <Typical 
             loop = {Infinity}
